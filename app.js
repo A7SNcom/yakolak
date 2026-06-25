f6403284f37e9f131dd5204605756cb51cc91df5
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-06-25 09:29:01 +0300

    Bump app to v073 lighting calibration

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import './src/fc.js?v=71';
+import './src/fc.js?v=73';
 
-console.info('[Yakolak] APP.JS v071 LOADED');
+console.info('[Yakolak] APP.JS v073 LIGHT CALIBRATION LOADED');
 
-const BUILD='71';
+const BUILD='73';
 import('./src/app-prod-stage1.js?v='+BUILD);
