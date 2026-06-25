061833e92d334fe8e6c8c82d451cb7df515a1842
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-06-25 09:52:42 +0300

    Enable orb light calibration

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import './src/room-black-patch.js?v=74';
+import './src/room-black-patch.js?v=75';
+import './src/lights.js?v=75';
 
-console.info('[Yakolak] APP.JS v074 FLOOR CEILING PATCH LOADED');
+console.info('[Yakolak] APP.JS v075 ORB LIGHT CALIBRATION LOADED');
 
-const BUILD='74';
+const BUILD='75';
 import('./src/app-prod-stage1.js?v='+BUILD);
