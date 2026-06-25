e2486ff5988d06db0514615a8a599835e57c50c6
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-06-25 11:40:50 +0300

    Bump app to v077 numeric light controls

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import './src/room-black-patch.js?v=76';
-import './src/lights.js?v=76';
+import './src/room-black-patch.js?v=77';
+import './src/lights.js?v=77';
 
-console.info('[Yakolak] APP.JS v076 COPY LIGHT VALUES LOADED');
+console.info('[Yakolak] APP.JS v077 NUMERIC ORB LIGHT CONTROLS LOADED');
 
-const BUILD='76';
+const BUILD='77';
 import('./src/app-prod-stage1.js?v='+BUILD);
