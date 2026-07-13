a691843e27186d813eed1214791f19a8211c49ec
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-07-13 15:37:55 +0300

    Load v094 interactive onboarding

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-console.info('[Yakolak] APP.JS v092 CALIBRATION TUTORIAL FIXES LOADED');
+console.info('[Yakolak] APP.JS v094 INTERACTIVE ONBOARDING LOADED');
 
-const BUILD='92';
-import('./src/app-game-v085.js?v='+BUILD);
+const BUILD='94';
+await import('./src/app-game-v085.js?v='+BUILD);
+await import('./src/onboarding-v094.js?v='+BUILD);
