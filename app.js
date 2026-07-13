49f57e4609839576d9c86bbc4fa7c71d5b11efbe
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-07-13 16:03:07 +0300

    Flag v094 before loading the game

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 console.info('[Yakolak] APP.JS v094 INTERACTIVE ONBOARDING LOADED');
 
+globalThis.__yakolakV094=true;
 const BUILD='94';
 await import('./src/app-game-v085.js?v='+BUILD);
 await import('./src/onboarding-v094.js?v='+BUILD);
