b7ecacda9c661974691a845948257bd6a7399886
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-07-26 00:08:03 +0300

    Load v121 mobile piece normals

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-console.info('[Yakolak] APP.JS v120 MOBILE BOARD SEPARATION LOADED');
+console.info('[Yakolak] APP.JS v121 MOBILE PIECE NORMALS LOADED');
 
-const BUILD='120';
+const BUILD='121';
 import('./src/mobile-clarity-v120.js?v='+BUILD+'-policy')
   .then(()=>import('./src/app-game-v114.js?v='+BUILD+'-release'))
+  .then(()=>import('./src/piece-normals-v121.js?v='+BUILD+'-normals'))
   .then(()=>import('./src/online-rounds-v118.js?v='+BUILD+'-rounds'))
   .then(()=>import('./src/online-last-move-v119.js?v='+BUILD+'-marker'))
   .catch(error=>{
-    console.error('[Yakolak] v120 bootstrap failed',error);
-    globalThis.__yakolakLoading?.set?.(100,'تعذر تحميل النسخة 120');
+    console.error('[Yakolak] v121 bootstrap failed',error);
+    globalThis.__yakolakLoading?.set?.(100,'تعذر تحميل النسخة 121');
   });
