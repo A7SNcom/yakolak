e0e79691a88edb20420ab7fa8ce56354847b0e5a
أحسن خليفة <100838084+A7SNcom@users.noreply.github.com>
2026-07-26 10:39:48 +0300

    Bust v121 wrapper cache after hotfix

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-console.info('[Yakolak] APP.JS v121 WALL ENTRY JOURNEY LOADED');
+console.info('[Yakolak] APP.JS v121 WALL ENTRY JOURNEY HOTFIX LOADED');
 
 const BUILD='121';
 import('./src/mobile-clarity-v120.js?v='+BUILD+'-policy')
-  .then(()=>import('./src/app-game-v121.js?v='+BUILD+'-entry'))
+  .then(()=>import('./src/app-game-v121.js?v='+BUILD+'-entry-hotfix-1'))
   .then(()=>import('./src/online-rounds-v118.js?v='+BUILD+'-rounds'))
   .then(()=>import('./src/online-last-move-v119.js?v='+BUILD+'-marker'))
   .catch(error=>{
